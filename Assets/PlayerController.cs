624f824cab5e7a2abc1a28b2559cd7decd309cf3
Pinukio <gkswjddnjs15@gmail.com>
2023-11-30 00:05:10 +0900

    add clear scene

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -55,5 +56,6 @@ public class PlayerController : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("°ñ");
+        SceneManager.LoadScene("ClearScene");
     }
 }
